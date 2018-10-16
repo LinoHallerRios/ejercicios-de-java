fbe58e3bc23362ff0d0dda10f4726d708d826c4d
Lino Haller Ríos <linohr00@gmail.com>
2018-10-16 14:15:40 +0200

    Update

--- Tema05/Ejercicio15.java
+++ Tema05/Ejercicio15.java
@@ -8,39 +8,39 @@ public class Ejercicio15 {
 
   public static void main(String[] args) {
     
-    /**
-    double baseDouble;
-    int expInt;
+    
     int exponente;
     double potencia;
     int cont=1;
     int cont2=0;
-    String base;
-    String exp;
+    double base;
+    int exp;
     
     System.out.printf("\nIntroduzca una base: ");
-    base =System.console().readLine();
-    baseDouble = Double.parseDouble(base);
+    base = Double.parseDouble(System.console().readLine());
     
     System.out.printf("\nIntroduzca un exponente: ");
-    exp= System.console().readLine();
-    expInt = Integer.parseInt(exp);
+    exp= Integer.parseInt(System.console().readLine());
 
 
-    while (cont <= expInt) {
+    while (cont <= exp) {
       
-      potencia = 1;
+      potencia = 0;
       exponente = cont;
       
       while (cont2 < exponente) {
       
-        potencia = potencia * baseDouble;
+        potencia = Math.pow(base,cont);
+        cont2++;
+        
       }
       
-      System.out.printf("\n%.2f ^ %d = %.2f",baseDouble,cont,potencia);
+      System.out.printf("\n%.2f ^ %d = %.2f",base,cont,potencia);
+      
+      cont++;
       
     }
-    */
+
 
   }
 }
